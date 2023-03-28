6c0f939da5494ddcc7d5d3105332a507a7a4cdbc
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-03-28 14:06:38 -0400

    Added selected-increase option

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-const items = document.querySelectorAll('.item')
-console.log(items);
+const items = document.querySelectorAll(".item");
+
+items.forEach(function (item) {
+  item.addEventListener("mouseover", function () {
+    removeFocus();
+    item.classList.add("selected-increase");
+  });
+});
+function removeFocus() {
+	items.forEach(function(item){
+		item.classList.remove("selected-increase");
+	})
+}
